1ff7ecfb798f405f4a43950b5114b85cedb22825
Aerron <cs15btech11040@iith.ac.in>
2019-04-25 19:20:24 +0530

    adding json to xml generator

---
+++ jsonreader.py
@@ -0,0 +1,23 @@
+import yedgraphml
+import json
+
+
+def jsonToxml(jsonfilename= "sample2.json", graphfilename="test.graphml"):
+	with open(jsonfilename) as f:
+	    data = json.load(f)
+	edge_map = {0: 'standard', 1: 'none'}
+	nodes = data['Nodes']
+	edges = data['Edges']
+	g = yedgraphml.Graph()
+
+	for node in nodes:
+		g.add_node(node['id'], label = node['name'], shape = node['shape'], shape_fill = node['color'], x=node['position'][0], y=node['position'][1])
+
+	for edge in edges:
+		edge['edge_type'] = edge_map[edge['edge_type']]
+		g.add_edge(edge['source'], edge['destination'], arrowhead = edge['edge_type'])
+
+
+	g.write_graph(graphfilename)
+
+	return graphfilename
